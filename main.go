2ee64eb0717933546846cf71bc92d476c8b71555
Takahiro <tk.iyei@gmail.com>
2021-07-26 21:55:09 +0900

    migrate v2

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 type responseBody struct {
@@ -19,51 +19,52 @@ type responseBody struct {
 }
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "deepl cli"
-	app.Usage = "Translate any text via deepl api."
-	app.Version = "0.0.1"
-	app.Action = func(c *cli.Context) error {
-		if len(c.Args()) > 1 {
-			fmt.Println("Please pass only one argument")
+	app := &cli.App{
+		Name:    "deepl cli",
+		Usage:   "Translate any text via deepl api.",
+		Version: "0.0.2",
+		Action: func(c *cli.Context) error {
+			if c.NArg() > 1 {
+				fmt.Println("Please pass only one argument")
+				return nil
+			}
+
+			client := &http.Client{
+				CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+			}
+
+			req, err := http.NewRequest("GET", "https://api-free.deepl.com/v2/translate", nil)
+			if err != nil {
+				panic(err)
+			}
+
+			q := req.URL.Query()
+			q.Add("text", c.Args().Get(0))
+			q.Add("source_lang", "JA")
+			q.Add("target_lang", "EN-US")
+			q.Add("auth_key", os.Getenv("DEEPL_AUTH_KEY"))
+			req.URL.RawQuery = q.Encode()
+
+			resp, err := client.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			var respBody responseBody
+			if err := json.Unmarshal(body, &respBody); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(respBody.Translations[0].Text)
 			return nil
-		}
-
-		client := &http.Client{
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return http.ErrUseLastResponse
-			},
-		}
-
-		req, err := http.NewRequest("GET", "https://api-free.deepl.com/v2/translate", nil)
-		if err != nil {
-			panic(err)
-		}
-
-		q := req.URL.Query()
-		q.Add("text", c.Args().Get(0))
-		q.Add("source_lang", "JA")
-		q.Add("target_lang", "EN-US")
-		q.Add("auth_key", os.Getenv("DEEPL_AUTH_KEY"))
-		req.URL.RawQuery = q.Encode()
-
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		var respBody responseBody
-		if err := json.Unmarshal(body, &respBody); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(respBody.Translations[0].Text)
-		return nil
+		},
 	}
 
 	err := app.Run(os.Args)
